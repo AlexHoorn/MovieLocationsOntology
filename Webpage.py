77a0cd1a6fb5c4943db24cdfe87e3c49036e661f
JoshuaSeth <SB@accette.eu>
2020-10-06 17:22:43 +0200

    Finished initial UI layout

--- Webpage.py
+++ Webpage.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.title("Songify")
 st.text("Or whatever name we choose")
+
+st.header("Choose your song preferences")
 
 # Temporary options
 # These will later be replaced by the real list of artist, real list of songs, etc.
@@ -19,5 +22,15 @@ st.slider("Select the period of the song", value=[
           1990, 2020], min_value=1920, max_value=2020)
 
 
+# Sidebar
 st.sidebar.header("Settings")
 st.sidebar.checkbox("Enable developer mode")
+
+
+st.header("Your recommendation")
+st.text("Temporary, to be replaced")
+
+# Temporary output
+components.html(
+    """<iframe src="https://open.spotify.com/embed/album/1zjw3IyO5IEaC8iu2GNwwA" width="600" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""",
+    height=800, scrolling=True)
