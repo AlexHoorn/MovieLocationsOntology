5bb86412f1bfa6f73a7498837c87255ffa7aa840
ramon284 <ramon.cremers@hotmail.com>
2020-10-12 22:16:01 +0200

    Added searching based on actor

--- application/tempUI.py
+++ application/tempUI.py
@@ -6,24 +6,6 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 ## The queries that are not inside a function are outdated, and use dbpedia instead of our local ontology, will be changed soon
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-
-sparql.setQuery(
-    """
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX yago: <http://dbpedia.org/class/yago/>
-    SELECT  * WHERE { 
-    ?actor a yago:Actor109765278;
-    rdfs:label ?actorLabel
-    FILTER(langMatches(lang(?actorLabel),"EN"))
-    } LIMIT 100
-"""
-)
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-actorList = [""]
-for result in results["results"]["bindings"]:
-    actorList.append(result["actorLabel"]["value"])
-
 sparql.setQuery(
     """
     PREFIX dbo: <http://dbpedia.org/ontology/>
@@ -98,24 +80,71 @@ def findMovie():
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     return movieList
 
+def findActor():
+    sparql = SPARQLWrapper("http://192.168.0.160:7200/repositories/projectTest")
+    sparql.setQuery(
+        """
+        PREFIX ex: <http://example.com/projectkand/>
+        select ?name where { 
+            ?actor a ex:Actor;
+                ex:hasFullName ?name  
+    } limit 100 
+    """
+    )
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    actorList = []
+    for result in results["results"]["bindings"]:
+        actorList.append(result["name"]["value"])
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    return actorList
+
+def findMovieActor(Actor): ## finds all movies with a specific actor in it
+    sparql = SPARQLWrapper("http://192.168.0.160:7200/repositories/projectTest")
+    sparql.setQuery(
+        """
+        PREFIX ex: <http://example.com/projectkand/>
+        select ?title where { 
+            ?actor ex:hasFullName '%s'@en.
+            ?movie a ex:Movie.
+            ?movie ex:hasCharacter ?character.
+            ?character ex:playedBy ?actor.
+            ?movie ex:hasPrimaryTitle ?title 
+    } limit 100 
+    """ 
+    % (Actor)
+    )## when i say "movie ex:hasCharacter" it doesn't work. It infers actors in protege, but not in graphdb...
+    print(Actor)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    movieActorList = []
+    for result in results["results"]["bindings"]:
+        movieActorList.append(result["title"]["value"])
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    return movieActorList
+
 
 ##########################################################################################################################
 
 st.title("Movie location finder")
 st.text("So that you can fall into the same vulcano as Gollum")
 
-## Multiple search options
-searchMode = st.radio(
-    "What do you want to search?", options=["Movies", "Actors", "Locations"]
-)
+
+
+#movieExpander = st.beta_expander("Movies", expanded=False)
+#actorExpander = st.beta_expander("Actors", expanded=False)
+#locationExpander = st.beta_expander("Locations", expanded=False)
+## I'm trying to add logic to the expanders (open 1, other 2 close), but can't find any documentation about it yet.
 
 ## User selects his favorite movie/actor/whatever
-if searchMode == "Movies":
+with st.beta_expander("Movies"):
+    #if searchMode == "Movies":
+
     movieList = findMovie()
-    inputMovie = st.selectbox("Select your favorite movie", movieList)
+    inputMovie = st.selectbox("Select your favorite movie", movieList, key='1')
     if inputMovie != "":  ## If movie is selected, render the scene selectbox
         sceneList, mappingCoordinates = findScene(inputMovie)
-        inputScene = st.selectbox("Select your scene", sceneList, index=0)
+        inputScene = st.selectbox("Select your scene", sceneList, key='2')
         if (
             inputScene != ""
         ):  ##if scene is selected, render the button to call the folium map
@@ -124,14 +153,36 @@ if searchMode == "Movies":
                     coordinates = [value[0], value[1]]
             m2 = folium.Map(location=coordinates, zoom_start=16)
             folium.Marker(coordinates, popup="test", tooltip="test").add_to(m2)
-            if st.button("test!"):
+            if st.button("test!", key='moviesButton'):
                 folium_static(m2)
 
-## searchMode actor and location don't use our own ontology yet and are kinda oudated.
 
-if searchMode == "Actors":
-    inputActor = st.selectbox("Select your favorite Actor", actorList, index=0)
-if searchMode == "Locations":
+with st.beta_expander("Actors"):
+#if searchMode == "Actors":
+    actorList = findActor()
+    inputActor = st.selectbox("Select your favorite Actor", actorList, key='3')
+    if inputActor != '':
+        movieActorList = findMovieActor(inputActor)
+        inputMovie2 = st.selectbox('select a movie', movieActorList, key='4')
+        if inputMovie2 != '':
+            sceneList, mappingCoordinates = findScene(inputMovie2)
+            inputScene = st.selectbox("Select your scene", sceneList, key='5')
+            if (
+                inputScene != ""
+            ):  ##if scene is selected, render the button to call the folium map
+                for key, value in mappingCoordinates.items():
+                    if key == inputScene:
+                        coordinates = [value[0], value[1]]
+                m2 = folium.Map(location=coordinates, zoom_start=16)
+                folium.Marker(coordinates, popup="test", tooltip="test").add_to(m2)
+                if st.button("test!", key='actorButton'):
+                    folium_static(m2)
+
+
+
+## searchmode locations is still kind of outdated and doesn't use our own graphdb endpoint
+with st.beta_expander("Locations"):
+#if searchMode == "Locations":
     inputLocation = st.selectbox("Select your location", locationList)
 ## These coordinates should come from querying our own ontology
 ## With the query being based on the user's selection ofcourse
