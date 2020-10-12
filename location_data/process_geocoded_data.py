0f163bff4189f11e3170af47453920c201c9d539
JoshuaSeth <SB@accette.eu>
2020-10-12 19:24:59 +0200

    finished merging and cleaning script

--- location_data/process_geocoded_data.py
+++ location_data/process_geocoded_data.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import os
 
-# UNFINISHED
-
-
+# HELPER FUNCTIONS
+##########################################
 def GetKeyByValue(myDict, myValue):
     for key, val in myDict:
         if val == myValue:
@@ -25,85 +24,121 @@ def MakeNANsEmpty(input):
     return input
 
 
-##########################################################################################################################
+def GetIdentifier(input, char, unique=True):
+    if unique:
+        return char + str(input)
+    else:
+        if input != "":
+            global index
+            index += 1
+            return char + str(index)
+        return ""
+
+
+# VARIABLES
+##########################################
 frames = []
 fileIndex = 0
-
+index = 0
 dir = os.getcwd() + "/location_data/raw_data_geocoded/"
-print(dir)
+
+# PROGRAM
+##########################################
 for subdirs, dirs, files in os.walk(dir):
-    #Track file
+    # Track file
     for file in files:
         print(fileIndex, "Processing file:", file, "\n")
         fileIndex += 1
-        
-        #Load CSV
-        loadedCSV = pd.read_csv(os.path.join(subdirs +"/" +file))
 
-        
+        # Load CSV
+        loadedCSV = pd.read_csv(os.path.join(subdirs + "/" + file))
 
         # # Make Nan values empty strings
         # loadedCSV["Scene"] = loadedCSV["Scene"].apply(MakeNANsEmpty)
 
-        #Append dataframe without its header
+        # Append dataframe without its header
         frames.append(loadedCSV[1:])
 
-#General
+# GENERAL
 ##########################################
 # Merge all df's
 merged = pd.concat(frames)
 
-#Drop index columns
-merged.drop(merged.columns[[0]],axis=1,inplace=True)
+# Drop index columns
+merged.drop(merged.columns[[0]], axis=1, inplace=True)
 
 
-#SHOW CODES
+# SHOWS
 ##########################################
 # Clean tconst
 merged["Code"] = merged["Code"].apply(CleanTitle)
 
-#Remove special characters from titles
+# Remove special characters from titles
 merged["Code"] = merged["Code"].apply(RemoveSpace)
 
-#Drop rows with column names that accidentally entered dataset
-merged.drop(merged.loc[merged['Code']=="Code"].index, inplace=True)
+# Drop rows with column names that accidentally entered dataset
+merged.drop(merged.loc[merged["Code"] == "Code"].index, inplace=True)
 
-#Rename code to tconst
-merged.rename({"Code":"tconst"}, inplace = True, axis="columns")
+# Rename code to tconst
+merged.rename({"Code": "tconst"}, inplace=True, axis="columns")
 
+# Rename column for show label
+merged.rename({"Show Name": "tLabel"}, inplace=True, axis="columns")
 
-#SHOW LABELS
+# SCENES
 ##########################################
-#Rename column for show label
-merged.rename({"Show Name":"tLabel"}, inplace = True, axis="columns")
-
-
-#SCENES
-##########################################
-#Remove special characters from scenes
+# Remove special characters from scenes
 merged["Scene"] = merged["Scene"].apply(RemoveSpace)
 
 # Get names for all rows with studios instead of scenes
-merged.drop(merged.loc[merged['Scene']=="(studio)"].index, inplace=True)
+merged.drop(merged.loc[merged["Scene"] == "(studio)"].index, inplace=True)
 
-#Make NAN ""
+# Make NAN ""
 merged["Scene"] = merged["Scene"].apply(MakeNANsEmpty)
 
+# Give all scenes identifier, same names are different scenes. e.g. multiple movies might have a "school" scene.
+merged["sconst"] = merged["Scene"].apply(lambda x: GetIdentifier(x, "s", False))
 
-#LOCATIONS
+# Rename scene label
+merged.rename({"Scene": "sLabel"}, inplace=True, axis="columns")
+
+# LOCATIONS
 ##########################################
-#Drop rows for which no location could be geocoded
-merged=merged[merged["geocodedLocation"].notnull()]
+# Drop rows for which no location could be geocoded
+merged = merged[merged["geocodedLocation"].notnull()]
 
-#Remove specials characters
+# Give locations a unique lconst
+merged = merged.assign(lconst=(merged["geocodedLocation"].astype("category").cat.codes))
+merged["lconst"] = merged["lconst"].apply(lambda x: GetIdentifier(x, "l"))
+
+# Remove specials characters
 merged["geocodedLocation"] = merged["geocodedLocation"].apply(RemoveSpace)
 
+# Rename lat and long
+merged.rename({"latitude": "lat", "longitude": "long"}, inplace=True, axis="columns")
 
+# Rename location label
+merged.rename({"geocodedLocation": "lLabel"}, inplace=True, axis="columns")
 
+# Rename original location label
+merged.rename({"Location": "lAltLabel"}, inplace=True, axis="columns")
+
+# Finally rearrange the columns
+merged = merged[
+    [
+        "tconst",
+        "tLabel",
+        "sconst",
+        "sLabel",
+        "lconst",
+        "lLabel",
+        "lAltLabel",
+        "lat",
+        "long",
+    ]
+]
 
 print(merged.shape)
 
 # Save
-merged.to_csv(os.getcwd()+"/location_data/allmerged.csv")
-
-    
+merged.to_csv(os.getcwd() + "/location_data/allmerged.csv")
