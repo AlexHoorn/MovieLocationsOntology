fa8f5fbdda9c4ae2a39bef0f551fddbb9d53c799
JoshuaSeth <SB@accette.eu>
2020-10-23 16:18:29 +0200

    add_owl_sameas progress

--- location_data/add_owl_same_as.py
+++ location_data/add_owl_same_as.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON
+import time
+from tqdm import tqdm
+import pickle
 
 #Quickly testing out some methods to do OWL:sameAs for our items
 #50000 movies / 150 unions per query = 333 queries
@@ -14,10 +17,19 @@ sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql"
 
 #Get triples
 def GetTriple(imdbID):
-    return """UNION { ?page wdt:P345 '"""+ imdbID +"""' } """
+    return """UNION { ?page wdt:P345 '"""+ imdbID +"""' .  ?page wdt:P345 ?title } """
 
+def save_obj(obj, name ):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+#Construct query for sectioon
 def GetQuery(imdbIDList):
-    query = """PREFIX wdt:<http://www.wikidata.org/prop/direct/> SELECT ?page WHERE { { ?page wdt:P345 'tt1528406' }"""
+    query = """PREFIX wdt:<http://www.wikidata.org/prop/direct/> SELECT ?title ?page WHERE { {  ?page wdt:P345 'tt1528406'. ?page wdt:P345 ?title }"""
     for id in imdbIDList.iteritems():
         query+=GetTriple(id[1])
     query+= """ } """
@@ -33,46 +45,71 @@ def GetWikiDataResource(query):
     results = sparql.query().convert()
     return results["results"]["bindings"]
 
+dictDir = os.getcwd() + "/owl_dicts/"
 readDir = os.getcwd() + "/allmerged.csv"
 
 df = pd.read_csv(readDir)
 
-df = df["tconst"].drop_duplicates()
+tconsts = df["tconst"].drop_duplicates()
 
-dfTop = df[:150]
-
-query = GetQuery(dfTop)
-
-print(query)
-
-results = GetWikiDataResource(query)
-
-# Print the results
-for result in results:
-    print(result["page"]["value"])
+currentDict = {}
 
 
 
+def TryGetOwlPages(rangeNrs, sleep, currentDict):
+    if not os.path.isfile(readDir.replace("allmerged.csv", "progress_dict.pkl")):
+        #Select a series of 80 items from the show titles
+        section = tconsts[rangeNrs:rangeNrs + 80]
 
-#Method 2 request the wikidata page for each imdb:id, gets a HTTP429 for too many requests after five requests. Rate limiting is going to take days again.
-# def GetWikiDataResource(imdbID):
-#     sparql.setQuery(
-#             """PREFIX wdt:<http://www.wikidata.org/prop/direct/> SELECT ?page WHERE { ?page wdt:P345 '"""+ imdbID +"""' } LIMIT 1 """
-#         )
+        #Create a query from this selection
+        query = GetQuery(section)
 
-#     sparql.setReturnFormat(JSON)
-#     results = sparql.query().convert()
-#     print(results["results"])
-#     print(results["results"]["bindings"])
-#     print(results["results"]["bindings"][0])
-#     return "https://www.wikidata.org/wiki/" + str(results["results"]["bindings"][0]["page"]["value"])
+        succeed = True
+        #Try to query the query to wikidata's sparql endpoint
+        try:
+            results = GetWikiDataResource(query)
+            #On succes default the sleep time again
+            sleep = 20
+        #If this does not work sleep and try again
+        except Exception as ex:
+            print(ex)
+            print("ERROR TRYING AGAIN")
+            time.sleep(sleep)
+            #Increment the sleep time and try again
+            succeed=False
+            #After 5 fails save progress and stop
+            if time > 120:
+                print("progress keeps failing, saving dict and stopping")
+                save_obj(currentDict, readDir.replace("allmerged.csv", "progress_dict"))
 
-# readDir = os.getcwd() + "/allmerged.csv"
+            TryGetOwlPages(rangeNrs, sleep + 20)
 
-# df = pd.read_csv(readDir)
+        #I do not want this to get executed after a retry
+        if succeed:
+            #Save results in a dict
 
-# df = df["tconst"].drop_duplicates()
+            #Assign results to a dict
+            for result in results:
+                currentDict[result["title"]["value"]]=result["page"]["value"]
+
+            #Print the dictionary to check if its right
+            print(currentDict)
+            save_obj(currentDict, dictDir+"dict" + str(rangeNrs))
 
 
-# for row in df.iteritems():
-#     print (GetWikiDataResource(row[1]))
+#Later we will map these dictionaries
+# #Map this dict to the original dat
+# df['Owl:SameAs'] = df['tconst'].map(currentDict)
+#
+# print("Sum of missing values: ",df['Owl:SameAs'].isna().sum())
+#
+# #save the progress
+# df.to_csv(readDir.replace("allmerged.csv", "zenodo_data.csv"))
+
+
+#Select a section from the data and query it
+for rangeNrs in tqdm(range(0, len(df), 80)):
+    TryGetOwlPages(rangeNrs, 20, currentDict)
+    time.sleep(8)
+
+
